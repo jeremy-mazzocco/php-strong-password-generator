39e10dd0d2815746c05d565143d664e8ef41171c
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-06-22 19:24:02 +0100

    add random pass

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+
+$parametro = $_GET['parametro'];
+function createpass($parametro)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+}
--- index.php
+++ index.php
@@ -6,35 +6,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>php passwords</title>
+
+    <?php
+
+    $length = $_GET['parametro'];
+
+    $capitalletters = range('A', 'Z');
+    $smalletters = range('a', 'z');
+    $numbers = range(0, 9);
+    $specials = ["!", "@", "#", "$", "%", "&", "*", "~"];
+
+    $joined = [...$capitalletters, ...$smalletters, ...$numbers, ...$specials];
+
+    $newpass = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomNu = rand(0, count($joined) - 1);
+        $randomLet = $joined[$randomNu];
+        $newpass .= $randomLet;
+    }
+
+
+    ?>
+
+    <?php include "functions.php"; ?>
 </head>
 
 <body>
     <div class="container">
-
+        <!-- title -->
         <h1>
             Strong Password Generator
         </h1>
         <h2>
             Genera una password sicura
         </h2>
-
+        <!-- sub-container -->
         <div class="sub-container">
             <form>
+
                 <div class="parametro">
-                    <input type="text" placeholder="nessun parametro vallido inserito" id="input-1" name="parametro" value="<?php echo $_GET['parametro']; ?>">
+                    <input type="number" placeholder="lunghezza password" id="input-1" name="parametro" value="<?php echo $_GET['parametro']; ?>">
                 </div>
+                <?php
+                echo $newpass
+
+                ?>
 
                 <div class="details">
-
                     <div class="password">
                         Lunghezza password: <input type="text">
-
                     </div>
-
                     <div class="options">
 
                     </div>
-
                     <div class="buttons">
                         <input type="submit" value="Invia">
                         <button>
@@ -42,6 +67,7 @@
                         </button>
                     </div>
                 </div>
+
             </form>
         </div>
     </div>
